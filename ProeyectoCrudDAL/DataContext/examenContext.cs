6248ba37d1a2ab898db3aaa3411b81baa537f9aa
ASAWERTCRUZ <vicoc123321@gmail.com>
2023-09-25 15:14:57 -0400

    CambiosAgregar archivos de proyecto.

---
+++ ProeyectoCrudDAL/DataContext/examenContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using ProeyectoCrud.Models;
+
+namespace ProeyectoCrud.DAL.DataContext
+{
+    public partial class examenContext : DbContext
+    {
+        public examenContext()
+        {
+        }
+
+        public examenContext(DbContextOptions<examenContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasIndex(e => e.Email, "UQ__Usuarios__A9D10534D078BD4F")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ciudad)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.FechaRegistro)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Genero)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Pais)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ProeyectoCrudDAL/Repositories/ContacRepository.cs
@@ -0,0 +1,38 @@
+﻿using ProeyectoCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCrud.DAL.Repositories
+{
+    public class ContacRepository : IGenericRepositorie<Contacto>
+    {
+        public Task<bool> Actualizar(Contacto modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insertar(Contacto modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contacto> Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<Contacto>> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
