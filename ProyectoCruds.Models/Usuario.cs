5e1981b32fb254c40d553f903bcc421b8057369f
ASAWERTCRUZ <vicoc123321@gmail.com>
2023-09-25 15:14:57 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoCruds.Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProeyectoCrud.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Genero { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Pais { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+    }
+}
